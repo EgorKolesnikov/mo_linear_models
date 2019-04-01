ce5dda461234195a274c869607f039da0ece6a84
Yahor Kalesnikau <kolegor@yandex-team.ru>
2019-04-01 22:45:33 +0300

    Little big of args parse

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,68 @@
 #include "reader.h"
 #include <iostream>
+#include <string.h>
 
 
-int main(int argc, char *argv[]){
-	test_f(argc, argv);
+enum class AlgoType {
+	sgd,
+	adagrad,
+	ftrl_proximal
+};
+
+
+struct ArgWrap{
+	AlgoType type;
+
+	ArgWrap()
+		: type(AlgoType::sgd)
+	{ }
+};
+
+
+int parse_args(int argc, char * argv[], ArgWrap& args){
+	if(argc == 1){
+		printf("Expected to have at least one argument: ['sgd', 'adagrad', 'ftrl-proximal']\n");
+		return 1;
+	}
+
+	if(strcmp(argv[1], "sgd") == 0){
+		args.type = AlgoType::sgd;
+	} else if(strcmp(argv[1], "adagrad") == 0) {
+		args.type = AlgoType::adagrad;
+	} else if(strcmp(argv[1], "ftrl-proximal") == 0){
+		args.type = AlgoType::ftrl_proximal;
+	} else {
+		printf("Unknown type '%s'\n", argv[1]);
+		return 2;
+	}
+
+	return 0;
+}
+
+int run(const ArgWrap& args){
+	if(args.type == AlgoType::sgd){
+		printf("Running sgd\n");
+	} else if(args.type == AlgoType::adagrad){
+		printf("Running adagrad\n");
+	} else if(args.type == AlgoType::ftrl_proximal){
+		printf("Running ftrl_proximal\n");
+	}
+	return 0;
+}
+
+
+int main(int argc, char * argv[]){
+	ArgWrap args;
+	
+	int rc_args = parse_args(argc, argv, args);
+	if(rc_args){
+		exit(rc_args);
+	}
+
+	int rc_run = run(args);
+	if(rc_run){
+		exit(rc_run);
+	}
+
 	return 0;
 }
